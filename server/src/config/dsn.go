61bf3ffc7b8791efa8300fd198b239b4ac2b22de
Kouta-fd <kotafd0124@icloud.com>
2022-12-09 20:25:31 +0900

    feat:room api

--- server/src/config/dsn.go
+++ server/src/config/dsn.go
@@ -2,31 +2,38 @@ package config
 
 import (
 	"fmt"
+	"log"
 	"os"
 )
 
 // DSNはdataSourceNameを返します、もし必須の環境変数が設定されてなかった場合はerrorを返します
 func DSN() (string, error) {
-	if os.Getenv("ENVIROMENT") == "PRODUCTION" {
-		return os.Getenv("POSTGRES_URL"), nil
+	log.Println(os.Getenv("ENVIRONMENT"))
+
+	if os.Getenv("ENVIRONMENT") == "prd" {
+		dbDSN := os.Getenv("DSN")
+		return dbDSN, nil
 	}
 
-	// DBの環境変数
-	dbHost := os.Getenv("POSTGRES_HOST")
-	dbPort := os.Getenv("POSTGRES_PORT")
-	dbUser := os.Getenv("POSTGRES_USER")
-	dbPassword := os.Getenv("POSTGRES_PASSWORD")
-	dbName := os.Getenv("POSTGRES_DB")
+	if os.Getenv("ENVIRONMENT") == "dev" {
+		dbUser := os.Getenv("DB_USER")
+		dbPassword := os.Getenv("DB_PASSWORD")
+		dbHost := os.Getenv("DB_HOST")
+		dbPort := os.Getenv("DB_PORT")
+		dbDatabase := os.Getenv("DB_DATABASE")
 
-	if dbUser == "" || dbPassword == "" || dbHost == "" || dbPort == "" || dbName == "" {
-		return "", fmt.Errorf("ERROR : required environment variable not found")
+		if dbUser == "" || dbPassword == "" || dbHost == "" || dbPort == "" || dbDatabase == "" {
+			return "", fmt.Errorf("ERROR : required environment variable not found")
+		}
+		return fmt.Sprintf(
+			"%s:%s@tcp(%s:%s)/%s?",
+			dbUser,
+			dbPassword,
+			dbHost,
+			dbPort,
+			dbDatabase,
+		) + "parseTime=true&collation=utf8mb4_bin", nil
 	}
-	return fmt.Sprintf(
-		"user=%s password=%s host=%s port=%s dbname=%s sslmode=disable",
-		dbUser,
-		dbPassword,
-		dbHost,
-		dbPort,
-		dbName,
-	), nil
+
+	return "", fmt.Errorf("Error : not match enviroment prd or dev , currently used %s", os.Getenv("ENVIRONMENT"))
 }
